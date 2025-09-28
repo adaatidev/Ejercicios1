f49a30daef9d2e54ef766d3a73c07e9dd256750b
chica <chica@UNMEI>
2025-09-28 20:57:40 +0200

    /2025/09/28/

---
+++ src/tema1/Ejercicio_9.java
@@ -0,0 +1,26 @@
+package tema1;
+
+import java.util.Scanner;
+
+public class Ejercicio_9 {
+
+	public static void main(String[] args) {
+		
+		int age;
+		boolean mayor;
+		
+		Scanner reader = new Scanner(System.in);
+		
+			System.out.print("Ingrese su edad:");
+			
+			age = reader.nextInt();
+		
+			mayor = (age >= 18);
+		
+			System.out.print("Mayor de edad:" + mayor);
+			
+			reader.close();
+			
+	}
+
+}
