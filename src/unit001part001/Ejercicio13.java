47c56814632421930fefb08413f89fd45368df38
chica <chica@UNMEI>
2025-09-29 19:53:58 +0200

    /2025/09/29/ ACTIVITIES_UNIT_001_PART_001

---
+++ src/unit001part001/Ejercicio13.java
@@ -0,0 +1,33 @@
+package unit001part001;
+
+import java.util.Scanner;
+
+public class Ejercicio13 {
+
+	public static void main(String[] args) {
+		
+		boolean rain;
+		boolean hw;
+		boolean lib;
+		boolean out;
+		
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.print("¿Está lloviendo?: ");
+		rain = reader.nextBoolean();
+		
+		System.out.print("¿Has hecho la tarea?: ");
+		hw = reader.nextBoolean();
+		
+		System.out.print("¿Tienes que ir a la biblioteca?: ");
+		lib = reader.nextBoolean();
+		
+		out = (!rain && hw);
+		
+		System.out.print("Sales: " + out);
+		
+		reader.close();
+
+	}
+
+}
---
+++ src/unit001part001/Ejercicio15.java
@@ -0,0 +1,24 @@
+package unit001part001;
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+
+	public static void main(String[] args) {
+		
+		final int IVA = 21;
+		double precio;
+		
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.print("Ingrese precio: ");
+		
+		precio = reader.nextDouble();
+		
+		System.out.print("Precio final con IVA: " + ((precio * IVA / 100) + precio));
+		
+		reader.close();
+
+	}
+
+}
